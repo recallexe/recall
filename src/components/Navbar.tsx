df124754ec9649cdaa0e408f75ba55253b55ea43
Ehsanullah Dehzad <>
2025-10-09 22:21:35 -0400

    add a dropdown menu for the user avatar for account management

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,25 @@
 import {
   Bell,
+  LogOut,
   MessageSquare,
   Network,
   PanelRight,
   Plus,
   Search,
+  Settings,
+  User2,
 } from "lucide-react";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Navbar() {
   return (
@@ -40,10 +51,32 @@ export default function Navbar() {
         <Button variant="outline">
           <Bell />
         </Button>
-        <Avatar>
-          <AvatarImage src="profile-pic.jpg" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+
+        {/* DROPDOWN MENU */}
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="profile-pic.jpg" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <User2 />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <Settings />
+                Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">
+                <LogOut />
+                Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
