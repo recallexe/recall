54d398331392236633c8efcba3242be517f85d76
Ehsanullah Dehzad <>
2025-10-09 22:12:03 -0400

    add the Navbar buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,50 @@
-import { Moon } from "lucide-react";
+import {
+  Bell,
+  MessageSquare,
+  Network,
+  PanelRight,
+  Plus,
+  Search,
+} from "lucide-react";
 import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
 
 export default function Navbar() {
   return (
     <nav className="p-4 flex items-center justify-between">
-        {/* LEFT */}
-        Collapse Button
-        {/* RIGHT */}
-        <div className="flex items-center gap-4">
-            <Link href="/">Dashboard</Link>
-            <Moon />
-        </div>
-    </nav>
-  )
+      {/* LEFT */}
+      <div className="flex items-center gap-4">
+        <Button variant="outline">
+          <PanelRight />
+        </Button>
+        <Button variant="outline">
+          <Search />
+          Search Anything
+        </Button>
+      </div>
 
+      {/* RIGHT */}
+      <div className="flex items-center gap-4">
+        <Button variant="outline">
+          <Plus />
+          New
+        </Button>
+        <Button variant="outline">
+          <Network />
+          Knowledge Graph
+        </Button>
+        <Button variant="outline">
+          <MessageSquare />
+        </Button>
+        <Button variant="outline">
+          <Bell />
+        </Button>
+        <Avatar>
+          <AvatarImage src="profile-pic.jpg" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </nav>
+  );
 }
