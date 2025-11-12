db9b412b9e8bdd7e6dd80c0654cde8d0c7be4c94
Fatima Fayazi <>
2025-11-11 22:49:58 -0500

    changing the area page.

--- src/app/dashboard/areas/page.tsx
+++ src/app/dashboard/areas/page.tsx
@@ -1,3 +1,5 @@
+import AddNewCard from "@/components/area/AddNewCard";
+import AreaCard from "@/components/area/AreaCard";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -10,7 +12,7 @@ export default function Areas() {
   return (
     <>
       <Breadcrumb>
-        <BreadcrumbList>
+        <BreadcrumbList className="text-2xl">
           <BreadcrumbItem>
             <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
           </BreadcrumbItem>
@@ -22,6 +24,40 @@ export default function Areas() {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+
+      <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
+        <AreaCard
+          title="Personal"
+          image="/personal.png"
+          href="/dashboard/areas/personal"
+        />
+        <AreaCard
+          title="Work"
+          image="/work.png"
+          href="/dashboard/areas/personal"
+        />
+        <AreaCard
+          title="Side Projects"
+          image="/project.png"
+          href="/dashboard/areas/personal"
+        />
+        <AreaCard
+          title="Health"
+          image="/health.png"
+          href="/dashboard/areas/personal"
+        />
+        <AreaCard
+          title="Finance"
+          image="/finance.png"
+          href="/dashboard/areas/personal"
+        />
+        <AreaCard
+          title="School"
+          image="/school.png"
+          href="/dashboard/areas/personal"
+        />
+        <AddNewCard></AddNewCard>
+      </div>
     </>
   );
 }
